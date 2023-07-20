aab3c4430b3e363ff93ae5563e185014f815e820
Sheena Tharakanparampil <sheena.tharakanparampil@harness.io>
2023-07-20 12:39:18 +0100

    Update main.tf

--- uri-default/main.tf
+++ uri-default/main.tf
@@ -14,12 +14,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "ec2test1" {
+resource "aws_instance" "ec2test2" {
   instance_type = var.instance_type
   ami           = "ami-0aa7d40eeae50c9a9"
 #   subnet_id = aws_subnet.main.id
   tags = {
-    name = "ec2test1"
+    name = "ec2test2"
     extra= var.extra
   }
 }
